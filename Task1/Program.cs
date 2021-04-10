50382e7e50f35d08dfa67ea807f559cd6a7d65a6
Yuriy Plyska <yuriyply@mail.com>
2021-04-10 16:06:11 +0300

    TheFirstTask

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,7 +7,7 @@ namespace Task1
         static void Main(string[] args)
         {
              new HomeWork().InvokePriceCalculatiion();
-            Console.ReadKey();
+             Console.ReadKey(true);
            
         }
     }
