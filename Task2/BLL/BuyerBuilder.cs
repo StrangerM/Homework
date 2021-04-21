844d41bf8385139c7de2f69150092bf97f032b72
Yuriy Plyska <yuriyply@mail.com>
2021-04-21 19:36:25 +0300

    Task3 has done

---
+++ Task2/BLL/BuyerBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Models;
+
+namespace Task2.BLL
+{
+    public class BuyerBuilder
+    {
+        
+        public Buyer CreateBuyer()
+        {
+            Buyer NewBuyer = new Buyer();
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Before to order of meal needs to fill in some data");
+                Console.WriteLine("Please input your Name  and press Enter");
+                NewBuyer.Name = Console.ReadLine();
+                if (String.IsNullOrEmpty(NewBuyer.Name))
+                {
+                    Console.WriteLine("Name can not be Empty");
+                    continue;
+                }
+                Console.WriteLine("Please input your Last Name  and press Enter");
+                NewBuyer.LastName = Console.ReadLine();
+                if (String.IsNullOrEmpty(NewBuyer.LastName))
+                {
+                    Console.WriteLine("Last Name can not be Empty");
+                    continue;
+                }
+                Console.WriteLine("Please input your Address and press Enter");
+                NewBuyer.Address = Console.ReadLine();
+                if (String.IsNullOrEmpty(NewBuyer.Address))
+                {
+                    Console.WriteLine("Address can not be Empty");
+                    continue;
+                }
+                Console.WriteLine("Please input your CellPhone, use format - 809........ and press Enter");
+                var cellPhone = Console.ReadLine();
+                var temp = 0;
+                if (!Int32.TryParse(cellPhone, out temp))
+                {
+                    Console.WriteLine("Please input number in the correct format");
+                    continue;
+                }
+                NewBuyer.CellPhone = temp.ToString();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Now you can choose meal");
+                return NewBuyer;
+            }
+
+        }
+    }
+}
---
+++ Task2/BLL/MealBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Models;
+
+namespace Task2.BLL
+{
+    public class MealBuilder
+    {
+        
+        public Meal CreateMeal()
+        {
+            Meal NewMeal = new Meal();
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Please add some meal");
+                Console.WriteLine("Please input  Name  and press Enter");
+                NewMeal.Name = Console.ReadLine();
+                if (String.IsNullOrEmpty(NewMeal.Name))
+                {
+                    Console.WriteLine("Name can not be Empty");
+                    continue;
+                }
+                Console.WriteLine("Please input Price $ (only number) and press Enter. Use . separator if need");
+                
+                var price = Console.ReadLine();
+                decimal temp = 0;
+                if (!decimal.TryParse(price, out temp))
+                {
+                    Console.WriteLine("Please input number in correct format");
+                    continue;
+                }
+                NewMeal.Price = temp;
+                Console.WriteLine("Please input Discount % (only number) if there is no discount -input - 0 and press Enter");
+
+                var dicount = Console.ReadLine();
+                var tempDiscount = 0;
+                if (!Int32.TryParse(dicount, out tempDiscount))
+                {
+                    Console.WriteLine("Please input number in correct format");
+                    continue;
+                }
+                NewMeal.Discount = tempDiscount;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Your Meal was add to DB successfuly");
+                return NewMeal;
+            }
+
+        }
+
+    }
+}
